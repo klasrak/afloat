c08338ac79c4044143f977a745215fb59e9458a0
Danilo Augusto <danilo.augusto@mercadolivre.com>
2024-01-24 17:00:10 -0300

    feat: add func to add float32

--- afloat.go
+++ afloat.go
@@ -1,1 +1,20 @@
 package afloat
+
+import (
+	"math"
+	"sync/atomic"
+	"unsafe"
+)
+
+func Add32(addr *float32, value float32) (result float32) {
+	for {
+		old := *addr
+		result = old + value
+
+		if atomic.CompareAndSwapUint32((*uint32)(unsafe.Pointer(addr)), math.Float32bits(old), math.Float32bits(result)) {
+			break
+		}
+	}
+
+	return
+}
