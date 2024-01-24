7d4e2d2717fb2c9f2f4e0a8b84a935f4b4179e7d
Danilo Augusto <danilo.augusto@mercadolivre.com>
2024-01-24 18:36:37 -0300

    feat: add godoc

--- afloat.go
+++ afloat.go
@@ -1,3 +1,4 @@
+// Package afloat provides atomic operations for float types
 package afloat
 
 import (
@@ -6,6 +7,9 @@ import (
 	"unsafe"
 )
 
+// Add32 adds value to *addr and returns the result.
+// It is implemented using atomic.CompareAndSwapUint32.
+// It is safe for concurrent use by multiple goroutines.
 func Add32(addr *float32, value float32) (result float32) {
 	for {
 		old := *addr
