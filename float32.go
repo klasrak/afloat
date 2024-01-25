c8fd2f58c8df2c3448d6412fed1191235116ab07
Danilo Augusto <danilo.augusto@mercadolivre.com>
2024-01-25 15:17:07 -0300

    feat: add Load32 function

--- float32.go
+++ float32.go
@@ -22,3 +22,10 @@ func Add32(addr *float32, value float32) (result float32) {
 
 	return
 }
+
+// Load32 returns the value at *addr.
+// It is implemented using atomic.LoadUint32.
+// It is safe for concurrent use by multiple goroutines.
+func Load32(addr *float32) (result float32) {
+	return math.Float32frombits(atomic.LoadUint32((*uint32)(unsafe.Pointer(addr))))
+}
--- float32_test.go
+++ float32_test.go
@@ -9,8 +9,8 @@ import (
 
 const (
 	MIN_F32       = math.SmallestNonzeroFloat32
-	MAX_F32       = math.MaxFloat32 / 10
-	TOLERANCE_F32 = 1e-12
+	MAX_F32       = 10.0
+	TOLERANCE_F32 = 1e-5 // 10^-5
 )
 
 func TestAdd32(t *testing.T) {
@@ -122,6 +122,70 @@ func TestAdd32(t *testing.T) {
 
 func getRandomFloat32() float32 {
 	return rand.Float32()*(MAX_F32-MIN_F32) + MIN_F32
+}
+
+func TestLoad32(t *testing.T) {
+
+	type testCase struct {
+		name     string
+		value    float32
+		maxLoads int
+	}
+
+	testCases := []testCase{
+		{
+			name:     "Load correct value",
+			value:    1.0,
+			maxLoads: 1,
+		},
+		{
+			name:     "Load correct value 100 times",
+			value:    1.0,
+			maxLoads: 100,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			wg := sync.WaitGroup{}
+			signalUpdate := make(chan struct{}, 1)
+
+			go func() {
+				for {
+					if _, ok := <-signalUpdate; ok {
+						Add32(&tc.value, 1.0)
+						continue
+					}
+					break
+				}
+			}()
+
+			testLoad := func() {
+
+				// Simulate concurrent access
+				signalUpdate <- struct{}{}  // signal an update, so the value changes
+				result := Load32(&tc.value) // load the value before/concurrently with the update
+
+				// Check if the result matches the expected value
+				if tc.value != result {
+					t.Errorf("Expected %.18f, got %.18f", tc.value, result)
+				}
+
+			}
+
+			for i := 0; i < tc.maxLoads; i++ {
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					testLoad()
+				}()
+
+			}
+
+			wg.Wait()
+			close(signalUpdate)
+		})
+	}
 }
 
 func float32Equals(a, b, tolerance float32) bool {
